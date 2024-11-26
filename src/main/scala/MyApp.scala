3a78ca9768f34ebf22b3b7f70b93e029a82d5714
Jonas Ho <jonas.ho@Live.com>
2024-11-26 21:00:00 +0800

    commit2: implemented readFile method to read the excel sheet

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,22 +1,57 @@
-
+import scala.io.Source
+import scala.collection.mutable.ListBuffer
 
 case class DataRow[T](
-                      date: String,
-                      state: String,
-                      beds: T,
-                      beds_covid: T,
-                      beds_noncrit: T,
-                      admitted_pui: T,
-                      admitted_total: T,
-                      discharged_covid: T,
-                      discharged_total: T,
-                      hosp_covid: T,
-                      hosp_pui: T,
-                      hosp_noncovid: T
+                       date: String,
+                       state: String,
+                       beds: T,
+                       beds_covid: T,
+                       beds_noncrit: T,
+                       admitted_pui: T,
+                       admitted_total: T,
+                       discharged_covid: T,
+                       discharged_total: T,
+                       hosp_covid: T,
+                       hosp_pui: T,
+                       hosp_noncovid: T
                      )
 
 object MyApp extends App {
 
+  def readFile[T](convert: String => T): List[DataRow[T]] =
+    val f = "src/main/resources/hospital.csv"
+    val file = Source.fromFile(f)
 
+    val lines = file.getLines().toList
+    val headers = if lines.nonEmpty then lines.head.split(",").map(_.trim.stripPrefix("\uFEFF")).toList else List.empty
+
+
+    def getIndex(headerName: String): Int =
+      val index = headers.indexOf(headerName)
+      if index == -1 then
+        throw new NoSuchElementException(s"Header '$headerName' not found in the CSV file.")
+      else
+        index
+
+    val hospitals = for line <- lines.tail yield
+      val values = line.split(",").map(_.trim)
+
+      DataRow(
+        date = values(getIndex("date")),
+        state = values(getIndex("state")),
+        beds = convert(values(getIndex("beds"))),
+        beds_covid = convert(values(getIndex("beds_covid"))),
+        beds_noncrit = convert(values(getIndex("beds_noncrit"))),
+        admitted_pui = convert(values(getIndex("admitted_pui"))),
+        admitted_total = convert(values(getIndex("admitted_total"))),
+        discharged_covid = convert(values(getIndex("discharged_covid"))),
+        discharged_total = convert(values(getIndex("discharged_total"))),
+        hosp_covid = convert(values(getIndex("hosp_covid"))),
+        hosp_pui = convert(values(getIndex("hosp_pui"))),
+        hosp_noncovid = convert(values(getIndex("hosp_noncovid")))
+      )
+    hospitals
+
+  val hospitalData = readFile(_.toInt)
 
 }
