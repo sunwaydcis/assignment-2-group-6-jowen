f9c6e5e868c3f227f78a2b843e44535ea4077bdd
Jonas Ho <jonas.ho@Live.com>
2024-11-26 20:44:17 +0800

    commit1: created case class DataRow

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,18 +1,18 @@
 
 
 case class DataRow[T](
-                    date: String,
-                    state: String,
-                    beds: T,
-                    beds_covid: T,
-                    beds_noncrit: T,
-                    admitted_pui: T,
-                    admitted_total: T,
-                    discharged_covid: T,
-                    discharged_total: T,
-                    hosp_covid: T,
-                    hosp_pui: T,
-                    hosp_noncovid: T
+                      date: String,
+                      state: String,
+                      beds: T,
+                      beds_covid: T,
+                      beds_noncrit: T,
+                      admitted_pui: T,
+                      admitted_total: T,
+                      discharged_covid: T,
+                      discharged_total: T,
+                      hosp_covid: T,
+                      hosp_pui: T,
+                      hosp_noncovid: T
                      )
 
 object MyApp extends App {
